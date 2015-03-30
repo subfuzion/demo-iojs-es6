1ba84efebd30d820a95e57a00f32522cf52a3a8c
Guy <guy@codefresh.io>
2015-03-30 12:53:05 +0300

    added auto start

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,7 @@
 wget https://iojs.org/dist/v1.6.2/iojs-v1.6.2-linux-x64.tar.gz
 tar zxf iojs-v1.6.2-linux-x64.tar.gz
-iojs-v1.6.2-linux-x64
+cd iojs-v1.6.2-linux-x64
 rm /usr/bin/node
-cp bin/* /usr/bin
+rm /usr/bin/npm
+ln -s /workspace/iojs-v1.6.2-linux-x64/bin/node /usr/bin/node
+ln -s /workspace/iojs-v1.6.2-linux-x64/bin/npm /usr/bin/npm
